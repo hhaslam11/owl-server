0b0bd8d8a9881fe8d48b895d5515ab823c5c2461
Maggie Holbling <maggieholbl@gmail.com>
2019-11-03 11:00:24 +0000

    update create action on letter, missing delivery date

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -7,7 +7,29 @@ class LettersController < ApplicationController
   end
 
   def create
-    create_params = letter_params.except("read", "user_id", "id")
+    create_params = letter_params.except("read", "user_id", "id", "from_country_code", "to_country_code")
+    
+    from_country = Country.where('abbreviation = ?', letter_params[:from_country_code])[0]
+    to_country = Country.where('abbreviation = ?', letter_params[:to_country_code])[0]
+
+    create_params[:sender_id] = letter_params[:user_id]
+    create_params[:from_country_id] = from_country.id
+    create_params[:to_country_id] = to_country.id
+
+    if create_params[:user_owl_id]
+      user_owl = UserOwl.where('user_id = ? AND owl_id = ?', letter_params[:user_id], create_params[:user_owl_id])[0]
+      owl = Owl.find(user_owl.owl_id)
+    else
+      user_owl = UserOwl.where('user_id = ? AND owl_id = ?', letter_params[:user_id], 1)[0]
+      owl = Owl.find(user_owl.owl_id)
+      create_params[:user_owl_id] = user_owl.id
+    end
+
+    if !create_params[:sent_date]
+      create_params[:sent_date] = Time.current
+    end
+
+    create_params[:delivery_date] = Time.current + 86400  #km * owl.speed
 
     letter = Letter.new(create_params)
 
@@ -28,7 +50,7 @@ class LettersController < ApplicationController
     letter.update!(letter_params.except("read", "user_id", "id"))
 
     if letter_params[:read]
-      letter.update!({pick_up_date: Time.now})
+      letter.update!({pick_up_date: Time.current})
     end
 
     render json: { status: 'SUCCESS', data: transform_letters('id', params[:id]) }
@@ -36,7 +58,7 @@ class LettersController < ApplicationController
 
   private
   def letter_params
-    params.permit(:user_id, :id, :sender_id, :from_country_id, :to_country_id, :user_owl_id, :receiver_id, :content, :sent_date, :delivery_date, :pick_up_date, :read)
+    params.permit(:user_id, :id, :from_country_code, :to_country_code, :user_owl_id, :content, :sent_date, :read)
   end
 
   private
@@ -106,6 +128,7 @@ class LettersController < ApplicationController
         id: letter.id,
         sender: sender,
         receiver: receiver,
+        user_owl_id: user_owl_id,
         content: letter.content,
         sent_date: letter.sent_date,
         delivery_date: letter.delivery_date,
