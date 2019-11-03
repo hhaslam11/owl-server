8b467aa502639cebabf23822c2e8020a4db662ae
Maggie <maggieholbl@gmail.com>
2019-11-03 01:04:00 -0700

    postoffice changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
     resources :letters, only: [:index, :create, :show, :update]
   end
 
+  
   put '/postoffice/:country_id/new' => 'postoffice/letters#update'
   patch '/postoffice/:country_id/new' => 'postoffice/letters#update'
 
   get '/country' => 'sessions#index'
-  
+
   post '/login' => 'sessions#create'
 end
