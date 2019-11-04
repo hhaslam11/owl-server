e119a202c636fb7db759b77a238d4f0f6c9a250c
Maggie Holbling <maggieholbl@gmail.com>
2019-11-04 06:51:23 +0000

    refactor code for letters controller

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -3,7 +3,9 @@ class LettersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    data = { sent_letters: transform_letters('sender_id', params[:user_id]), received_letters: transform_letters('receiver_id', params[:user_id]) }
+    data = { sent_letters: LettersHelper.transform_letters('sender_id', params[:user_id]),
+              received_letters: LettersHelper.transform_letters('receiver_id', params[:user_id])
+            }
     render json: { status: 'SUCCESS', data: data }
   end
 
@@ -30,7 +32,7 @@ class LettersController < ApplicationController
       create_params[:sent_date] = Time.current
     end
 
-    create_params[:delivery_date] = deliverySetting(from_country, to_country, owl) # 86400  #km * owl.speed
+    create_params[:delivery_date] = LettersHelper.deliverySetting(from_country, to_country, owl) # 86400  #km * owl.speed
 
     letter = Letter.new(create_params)
 
@@ -43,7 +45,7 @@ class LettersController < ApplicationController
 
   # not using user id at all - authentication?
   def show
-    render json: { status: 'SUCCESS', data: transform_letters('id', params[:user_id]) }
+    render json: { status: 'SUCCESS', data: LettersHelper.transform_letters('id', params[:user_id]) }
   end
 
   def update
@@ -54,101 +56,12 @@ class LettersController < ApplicationController
       letter.update!({pick_up_date: Time.current})
     end
 
-    render json: { status: 'SUCCESS', data: transform_letters('id', params[:id]) }
+    render json: { status: 'SUCCESS', data: LettersHelper.transform_letters('id', params[:id]) }
   end
 
   private
   def letter_params
     params.permit(:user_id, :id, :from_country_code, :to_country_code, :user_owl_id, :content, :sent_date, :read)
   end
-
-  private
-  def is_read(pick_up_date)
-    if pick_up_date
-      return true
-    end
-    false
-  end
-
-  private
-  def deliverySetting(country1, country2, owl)
-    if !country1.lat || !country1.lon || !country2.lat || !country2.lon
-      time = 86400
-    else
-      puts LettersHelper.distance(country1.lat, country1.lon, country2.lat, country2.lon)
-      puts owl.speed 
-      puts time = LettersHelper.distance(country1.lat, country1.lon, country2.lat, country2.lon) / (1000 * owl.speed) * 3600
-      time = LettersHelper.distance(country1.lat, country1.lon, country2.lat, country2.lon) / (1000 * owl.speed) * 3600
-    end
-    Time.current + time
-  end
-
-  private
-  def transform_letters(id, param)
-    letters = Letter.where("#{id} = ?", param)
-    return letters.map { |letter|
-
-      if Country.exists?(id: letter.from_country_id)
-        from_country = Country.find(letter.from_country_id)
-
-        sending_country = {
-          id: from_country.id,
-          name: from_country.name,
-          abbreviation: from_country.abbreviation,
-          timezone: from_country.timezone,
-          flag_image: from_country.flag_image,
-          languages: from_country.languages
-        }
-      end
-
-      if Country.exists?(id: letter.to_country_id)
-        to_country = Country.find(letter.to_country_id)
-
-        receiving_country = {
-          id: to_country.id,
-          name: to_country.name,
-          abbreviation: to_country.abbreviation,
-          timezone: to_country.timezone,
-          flag_image: to_country.flag_image,
-          languages: to_country.languages
-        }
-      end
-
-      if User.exists?(id: letter.receiver_id)
-        to_user = User.find(letter.receiver_id)
-
-        receiver = {
-        id: to_user.id,
-        email: to_user.email,
-        username: to_user.username,
-        avatar: to_user.avatar,
-        country: receiving_country
-      }
-      end
-
-      if User.exists?(id: letter.sender_id)
-        from_user = User.find(letter.sender_id)
-      
-        sender = {
-          id: from_user.id,
-          email: from_user.email,
-          username: from_user.username,
-          avatar: from_user.avatar,
-          country: sending_country
-        }
-      end
-
-      {
-        id: letter.id,
-        sender: sender,
-        receiver: receiver,
-        user_owl_id: letter.user_owl_id,
-        content: letter.content,
-        sent_date: letter.sent_date,
-        delivery_date: letter.delivery_date,
-        pick_up_date: letter.pick_up_date,
-        read: is_read(letter.pick_up_date)
-      }
-    }
-  end
+  
 end
--- app/helpers/letters_helper.rb
+++ app/helpers/letters_helper.rb
@@ -15,4 +15,88 @@ module LettersHelper
     d = (r * c).round
   end
 
+  def LettersHelper.deliverySetting(country1, country2, owl)
+    if !country1.lat || !country1.lon || !country2.lat || !country2.lon
+      time = 86400
+    else
+      time = self.distance(country1.lat, country1.lon, country2.lat, country2.lon) / (1000 * owl.speed) * 3600
+    end
+    Time.current + time
+  end
+
+  def LettersHelper.is_read(pick_up_date)
+    if pick_up_date
+      return true
+    end
+    false
+  end
+
+  def LettersHelper.transform_letters(id, param)
+    letters = Letter.where("#{id} = ?", param)
+    return letters.map { |letter|
+
+      if Country.exists?(id: letter.from_country_id)
+        from_country = Country.find(letter.from_country_id)
+
+        sending_country = {
+          id: from_country.id,
+          name: from_country.name,
+          abbreviation: from_country.abbreviation,
+          timezone: from_country.timezone,
+          flag_image: from_country.flag_image,
+          languages: from_country.languages
+        }
+      end
+
+      if Country.exists?(id: letter.to_country_id)
+        to_country = Country.find(letter.to_country_id)
+
+        receiving_country = {
+          id: to_country.id,
+          name: to_country.name,
+          abbreviation: to_country.abbreviation,
+          timezone: to_country.timezone,
+          flag_image: to_country.flag_image,
+          languages: to_country.languages
+        }
+      end
+
+      if User.exists?(id: letter.receiver_id)
+        to_user = User.find(letter.receiver_id)
+
+        receiver = {
+        id: to_user.id,
+        email: to_user.email,
+        username: to_user.username,
+        avatar: to_user.avatar,
+        country: receiving_country
+      }
+      end
+
+      if User.exists?(id: letter.sender_id)
+        from_user = User.find(letter.sender_id)
+      
+        sender = {
+          id: from_user.id,
+          email: from_user.email,
+          username: from_user.username,
+          avatar: from_user.avatar,
+          country: sending_country
+        }
+      end
+
+      {
+        id: letter.id,
+        sender: sender,
+        receiver: receiver,
+        user_owl_id: letter.user_owl_id,
+        content: letter.content,
+        sent_date: letter.sent_date,
+        delivery_date: letter.delivery_date,
+        pick_up_date: letter.pick_up_date,
+        read: self.is_read(letter.pick_up_date)
+      }
+    }
+  end
+
 end
