c36901af13b2ec3e396d42901d18f1e18ec194ba
Maggie Holbling <maggieholbl@gmail.com>
2019-11-04 08:05:30 +0000

    get country data from ip address

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,19 @@ class SessionsController < ApplicationController
   skip_before_action :verify_authenticity_token
   
   def index
-    #might have to sign up for a token (param for create)
-    #for Kaleb to test
-    handler = IPinfo::create
-    details = handler.details(request.remote_ip)
-    country = details.country
+    access_token = "#{ENV['IPINFO_TOKEN']}"
+    puts request.env['HTTP_X_FORWARDED_FOR']
+    handler = IPinfo::create(access_token)
     
-    #TO DO: request.env['HTTP_X_FORWARDED_FOR']
+    details = handler.details(request.remote_ip)
+    
+    #testing for a proxy
+    if !details.respond_to?(:country)
+      details = handler.details(request.env['HTTP_X_FORWARDED_FOR'])
+    end
 
-    if details.nil?
+    if !details.respond_to?(:country)
       render json: { status: 'ERROR', data: "#{request.remote_ip} was not found in the database" }
-
     else
       country = Country.where('abbreviation = ?', details.country)
       render json: { status: 'SUCCESS', data: country}
