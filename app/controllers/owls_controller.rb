20cdaf6148b7f07031df6d5c9556679404375899
Maggie <maggieholbl@gmail.com>
2019-11-07 21:10:56 +0000

    restructure owl route

--- app/controllers/owls_controller.rb
+++ app/controllers/owls_controller.rb
@@ -5,12 +5,52 @@ class OwlsController < ApplicationController
     user_owls = UserOwl.where('user_id = ?', params[:user_id])
     
     user_owls.each do |user_owl|
-      # letters = Letter.where('user_owl_id = ?', user_owl.id)
+      letters = Letter.where('user_owl_id = ?', user_owl.id)
+
+      return letters.map { |letter|
+
+        if Country.exists?(id: letter.to_country_id)
+          to_country = Country.find(letter.to_country_id)
+
+          receiving_country = {
+            id: to_country.id,
+            name: to_country.name,
+            abbreviation: to_country.abbreviation,
+          }
+        end
+
+        if Country.exists?(id: letter.from_country_id)
+          from_country = Country.find(letter.from_country_id)
+
+          sending_country = {
+            id: from_country.id,
+            name: from_country.name,
+            abbreviation: from_country.abbreviation,
+          }
+        end
+
+        {
+          id: letter.id,
+          sending_country: sending_country,
+          receiving_country: receiving_country,
+          sender: letter.sender_id,
+          receiver: letter.receiver_id,
+          user_owl_id: letter.user_owl_id,
+          content: letter.content,
+          sent_date: letter.sent_date,
+          delivery_date: letter.delivery_date,
+          pick_up_date: letter.pick_up_date,
+          read: self.is_read(letter.pick_up_date)
+        }
+
+      }
+    end
+
       owl = Owl.where('id = ?', user_owl.owl_id)
       data.push({
         user_owls_id: user_owl.id,
         owl: owl,
-        letters: LettersHelper.transform_letters('user_owl_id', user_owl.id)
+        letters: letters
       })
     end
     
